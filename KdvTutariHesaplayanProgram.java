ff2e2eeb25bc72ec0115e85d73ac9ead8f886948
1whiterabbit <98350979+1whiterabbit@users.noreply.github.com>
2022-03-13 16:39:30 +0300

    Update KdvTutariHesaplayanProgram.java

--- KdvTutariHesaplayanProgram.java
+++ KdvTutariHesaplayanProgram.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class KdvTutariHesaplayanProgram {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        double urunFiyati,kdvTutari,urununKdvsi,kdvliUrunFiyati;
+        double urunFiyati,kdvOrani,urununKdvsi,kdvliUrunFiyati;
         System.out.print("Ürün Fiyatını Giriniz : ");
         urunFiyati = input.nextInt();
-        kdvTutari = 0.18;
-        System.out.println("Kdv oranı : % 18 ");
-        urununKdvsi = urunFiyati*kdvTutari;
+        kdvOrani = (urunFiyati<1000) ? 0.18 : 0.08;
+        System.out.println("Kdv oranı : " + kdvOrani);
+        urununKdvsi = urunFiyati*kdvOrani;
         System.out.println("Ürünün Kdv Tutarı : " + urununKdvsi + " tl");
         kdvliUrunFiyati = urunFiyati + urununKdvsi;
         System.out.println("Kdv'li Ürün Fiyatı : " + kdvliUrunFiyati + " tl");
