e4ba5efe9ef75ab843e1600cd102edef69daa3f4
1whiterabbit <98350979+1whiterabbit@users.noreply.github.com>
2022-03-21 23:08:11 +0300

    Create AsalSayilariBulanProgram.java

---
+++ AsalSayilariBulanProgram.java
@@ -0,0 +1,16 @@
+public class AsalSayilariBulanProgram {
+    public static void main (String[] args) {
+        int sayac;
+        for (int i=2;i<=100;i++){
+            sayac=0;
+            for (int j=2;j<i;j++){
+                if(i%j==0){
+                    sayac++;
+                }
+            }
+            if(sayac==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
